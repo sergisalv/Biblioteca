d019966dac2abf3d6c1f6804f45b0d027ebcc3f5
sergisalv <sergisalv@gmail.com>
2024-09-16 12:19:52 +0200

    Inicio administrador biblioteca

---
+++ biblioteca/src/test/java/com/sergisalv/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergisalv.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
