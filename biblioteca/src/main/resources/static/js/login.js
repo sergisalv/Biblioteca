8b685a75492023932576ab00025df356739cd8e9
sergisalv <sergisalv@gmail.com>
2024-09-29 10:57:14 +0200

    Cambio método para redirigir desde el login

--- biblioteca/src/main/resources/static/js/login.js
+++ biblioteca/src/main/resources/static/js/login.js
@@ -23,14 +23,32 @@ async function login() {
 
    sessionStorage.token = token;
 
-    if(email == "admin@admin.com"){
-    window.location.href = 'administrador.html'
-   }else{
-    window.location.href = 'usuario.html'
-   }
+    isAdministrador();
+   
 
 
 
 
 
 }
+
+async function isAdministrador(){
+    let url = 'http://localhost:8080/api/' + 'auth/administrator';
+
+    let config = {
+        method: 'GET',
+        headers: {
+            'Content-Type' : 'application/json',
+            'Authorization' : sessionStorage.token 
+        }
+    }
+
+
+    let response = await fetch(url, config);
+    let administrator = await response.text();
+    if (administrator.includes(true)){
+      window.location.href = 'administrador.html'
+    }else{
+       window.location.href = 'usuario.html'
+    }
+ }
